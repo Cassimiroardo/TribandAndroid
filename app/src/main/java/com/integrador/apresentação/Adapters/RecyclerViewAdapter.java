203ec8082ea9a623588e7eac0121dec036746ed6
cassimiro <edu27.cassimiro@gmail.com>
2019-11-16 17:52:31 -0300

    problema do gradle resolvido

--- app/src/main/java/com/integrador/apresentação/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/integrador/apresentação/Adapters/RecyclerViewAdapter.java
@@ -1,9 +1,9 @@
 package com.integrador.apresentação.Adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
--- app/src/main/java/com/integrador/apresentação/Interfaces/PesquisarActivity.java
+++ app/src/main/java/com/integrador/apresentação/Interfaces/PesquisarActivity.java
@@ -1,7 +1,7 @@
 package com.integrador.apresentação.Interfaces;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.integrador.apresetação.R;
 
