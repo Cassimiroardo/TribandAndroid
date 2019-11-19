10ddeb07bd6e03c25bf04d02c82bd6fa34105471
Erik Morbach <morbacherik@gmail.com>
2019-11-18 22:19:03 -0300

    Chat Terminado mas não testado

--- app/src/main/java/com/integrador/apresentação/Interfaces/ChatActivity.java
+++ app/src/main/java/com/integrador/apresentação/Interfaces/ChatActivity.java
@@ -3,6 +3,8 @@ package com.integrador.apresentação.Interfaces;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ import com.integrador.model.utilitarios.Usuario;
 import com.integrador.services.MensagemService;
 import com.integrador.services.RetrofitUtils;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,30 +35,109 @@ public class ChatActivity extends AppCompatActivity {
     private Button bt_sair;
     private EditText et_mensagem;
     private Button bt_envia;
+    private ArrayAdapter<Mensagem> mensagemAdapter;
 
+    private void inicializaComponentes(){
+        chat = (Chat) getIntent().getSerializableExtra("chat");
+        if(getIntent().getStringExtra("tipo").equals("estudio")) {
+            local = chat.getEstudio();
+            web = chat.getBanda();
+        }
+        else{
+            local = chat.getBanda();
+            web = chat.getEstudio();
+        }
+
+        tv_nome_web = findViewById(R.id.tv_nome_web);
+        bt_sair = findViewById(R.id.bt_sair);
+        et_mensagem = findViewById(R.id.et_mensagem);
+        bt_envia = findViewById(R.id.bt_envia);
+
+        service.buscarPorChat(chat).enqueue(new Callback<List<Mensagem>>() {
+            @Override
+            public void onResponse(Call<List<Mensagem>> call, Response<List<Mensagem>> response) {
+                if(response.isSuccessful())
+                    mensagens = (ArrayList) response.body();
+                else
+                    mensagens = new ArrayList<>();
+
+
+
+                mensagemAdapter = new ArrayAdapter<Mensagem>(ChatActivity.this,android.R.layout.simple_list_item_1,mensagens);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Mensagem>> call, Throwable t) {
+
+            }
+        });
+
+        tv_nome_web.setText(web.getNome());
+
+
+
+        bt_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        bt_envia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mensagem = et_mensagem.getText().toString();
+                if(mensagem.length()==0) return;
+
+                Mensagem nova = new Mensagem();
+                nova.setConteudo(mensagem);
+                nova.setHorario(null);
+                nova.setChat(chat);
+                service.adicionar(nova).enqueue(new Callback<Mensagem>() {
+                    @Override
+                    public void onResponse(Call<Mensagem> call, Response<Mensagem> response) {
+                        if(response.isSuccessful())
+                            mensagemAdapter.add(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<Mensagem> call, Throwable t) {
+
+                    }
+                });
+
+
+            }
+        });
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        inicializaComponentes();
 
         Thread contador = new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
-                    service.buscarPorChat(chat).enqueue(new Callback<List<MensagemService>>() {
+                    service.buscarPorChat(chat).enqueue(new Callback<List<Mensagem>>() {
                         @Override
-                        public void onResponse(Call<List<MensagemService>> call, Response<List<MensagemService>> response) {
+                        public void onResponse(Call<List<Mensagem>> call, Response<List<Mensagem>> response) {
                             if (response.isSuccessful()) {
                                 ArrayList<Mensagem> novas = (ArrayList) response.body();
 
                                 for (int i = mensagens.size(); i < novas.size(); i++) {
                                     mensagens.add(novas.get(i));
                                 }
+
+
                             }
                         }
 
                         @Override
-                        public void onFailure(Call<List<MensagemService>> call, Throwable t) {
+                        public void onFailure(Call<List<Mensagem>> call, Throwable t) {
                         }
                     });
                     try {
