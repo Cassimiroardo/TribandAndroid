2be609f0a5fe6484a7100af34cf5001ccae67e3a
Cassimiroardo <edu27.cassimiro@gmail.com>
2019-11-18 21:02:19 -0300

    tela de editar a banda pronto, show

--- app/src/main/java/com/integrador/apresentação/Interfaces/EditarBandaActivity.java
+++ app/src/main/java/com/integrador/apresentação/Interfaces/EditarBandaActivity.java
@@ -5,18 +5,30 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.integrador.apresetação.R;
 import com.integrador.model.classes.Banda;
 import com.integrador.services.BandaService;
 import com.integrador.services.RetrofitUtils;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
 import com.mobsandgeeks.saripaar.annotation.Password;
 
-public class EditarBandaActivity extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditarBandaActivity extends AppCompatActivity implements Validator.ValidationListener {
 
     private EditText etNome;
     private EditText etEmail;
@@ -35,34 +47,32 @@ public class EditarBandaActivity extends AppCompatActivity {
     private Banda banda;
     private BandaService bandaService;
 
+    private Validator validator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_banda);
         inicializa();
-        edit();
 
-    }
 
-    private void edit(){
-
-        this.btSalvar.setOnClickListener(new View.OnClickListener() {
+        btSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-
-
-
-
-
-                Intent intent = new Intent(EditarBandaActivity.this,PerfilBandaActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                edit();
             }
         });
 
     }
 
-    private void inicializa(){
+    private void edit() {
+        this.validator.validate();
+    }
+
+    private void inicializa() {
+        this.validator = new Validator(this);
+        this.validator.setValidationListener(this);
+
         this.banda = (Banda) getIntent().getSerializableExtra("banda");
         this.bandaService = RetrofitUtils.retrofit.create(BandaService.class);
 
@@ -76,4 +86,51 @@ public class EditarBandaActivity extends AppCompatActivity {
         this.btSalvar = findViewById(R.id.bt_salvar);
     }
 
+    @Override
+    public void onValidationSucceeded() {
+        if (etSenhaAtual.getText().toString().equals(banda.getSenha())) {
+
+
+            banda.setEmail(etEmail.getText().toString());
+            banda.setNome(etNome.getText().toString());
+            banda.setSenha(etNovaSenha.getText().toString());
+            banda.setIntegrantes(Integer.parseInt(etNumIntegrantes.getText().toString()));
+
+
+            Call<Banda> call = bandaService.atualizar(banda);
+
+            call.enqueue(new Callback<Banda>() {
+                @Override
+                public void onResponse(Call<Banda> call, Response<Banda> response) {
+
+                    if(response.isSuccessful()) {
+                        Intent intent = new Intent(EditarBandaActivity.this, PerfilBandaActivity.class);
+                        banda = response.body();
+                        intent.putExtra("banda", banda);
+                        Toast.makeText(EditarBandaActivity.this, "Dados editados!!!", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                        }
+                    }
+
+                @Override
+                public void onFailure(Call<Banda> call, Throwable t) {
+                    Toast.makeText(EditarBandaActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } else {
+            Toast.makeText(EditarBandaActivity.this, "Senha Atual incorreta!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError erro : errors) {
+            View componente = erro.getView();
+            String msg = erro.getCollatedErrorMessage(this);
+            if (componente instanceof EditText) {
+                ((TextView) componente).setError(msg);
+            }
+        }
+    }
 }
--- app/src/main/java/com/integrador/apresentação/Interfaces/LoginEstudioActivity.java
+++ app/src/main/java/com/integrador/apresentação/Interfaces/LoginEstudioActivity.java
@@ -6,15 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.integrador.apresetação.R;
+import com.integrador.model.classes.Estudio;
+import com.integrador.services.EstudioService;
+import com.integrador.services.RetrofitUtils;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Password;
 
-public class LoginEstudioActivity extends AppCompatActivity {
+import java.util.List;
 
-    private ImageView ivLogo;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginEstudioActivity extends AppCompatActivity implements Validator.ValidationListener{
+
+    private Estudio estudio;
+    private EstudioService estudioService;
+    private Validator validator;
+
+    @Email(message = "Email Obrigatório")
     private EditText etEmail;
+
+    @Password(message = "Senha Obrigatória")
     private EditText etSenha;
+
     private Button btLogar;
     private Button btCadastrar;
 
@@ -26,8 +46,8 @@ public class LoginEstudioActivity extends AppCompatActivity {
 
         this.btLogar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-              logar();
+            public void onClick(View view) {
+                logar();
             }
         });
 
@@ -42,17 +62,59 @@ public class LoginEstudioActivity extends AppCompatActivity {
     }
 
     private void inicializa(){
-        this.ivLogo = findViewById(R.id.iv_logo);
+        this.validator = new Validator(this);
+        this.validator.setValidationListener(this);
+        this.estudio = new Estudio();
+        this.estudioService = RetrofitUtils.retrofit.create(EstudioService.class);
+
         this.etEmail = findViewById(R.id.et_email);
         this.etSenha = findViewById(R.id.et_senha);
         this.btLogar = findViewById(R.id.bt_logar);
         this.btCadastrar = findViewById(R.id.bt_cadastrar);
     }
 
-    public void logar(){
-        Intent intent = new Intent(this, PerfilEstudioActivity.class);
-        intent.putExtra("tipo",1);
-        startActivity(intent);
+    private void logar(){
+        this.validator.validate();
     }
 
+    @Override
+    public void onValidationSucceeded() {
+        this.btLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Call<Estudio> call = estudioService.buscarPorEmailEsenha(etEmail.getText().toString(),etSenha.getText().toString());
+
+                call.enqueue(new Callback<Estudio>() {
+                    @Override
+                    public void onResponse(Call<Estudio> call, Response<Estudio> response) {
+
+                        if (response.isSuccessful()) {
+                            estudio = response.body();
+
+                            Intent intent = new Intent(LoginEstudioActivity.this,PerfilEstudioActivity.class);
+
+                            intent.putExtra("estudio",estudio);
+
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginEstudioActivity.this, "Login Invalido!!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Estudio> call, Throwable t) {
+                        Toast.makeText(LoginEstudioActivity.this, "Login invalido!!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+
+    }
 }
